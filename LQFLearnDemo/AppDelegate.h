8ca9518b659014406c9546326519e53f98c94f07
ThinkerLqf <804024833@qq.com>
2017-07-13 10:50:12 +0800

    Initial Commit

---
+++ LQFLearnDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  LQFLearnDemo
+//
+//  Created by liqunfei on 2017/7/13.
+//  Copyright © 2017年 LQF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ LQFLearnDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LQFLearnDemo
+//
+//  Created by liqunfei on 2017/7/13.
+//  Copyright © 2017年 LQF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
